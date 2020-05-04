15a34d5819d0f076144963f2c12a815e09c0df12
Colm Britton <colmjude@gmail.com>
2020-05-04 21:03:44 +0100

    Make note making script more robust

--- bin/create_til.py
+++ bin/create_til.py
@@ -12,21 +12,34 @@ author: Colm Britton
 """
 
 
-til_dir = "docs/"
+base_dir = "docs/"
 
 
 def title_to_filename(title):
     return title.lower().replace(" ", "-")
 
 
-def create_file(title):
-    til_file = open(os.path.join(til_dir, f"{title_to_filename(title)}.md"), 'w')
-    til_file.write(frontmatter_str.replace("<<title>>", title))
-    til_file.close()
+def create_file(args):
+    til_dir = base_dir
+
+    if len(args) > 1:
+        til_dir = til_dir + args[1]
+
+    if not os.path.isdir(til_dir):
+        os.mkdir(til_dir)
+
+    til_path = os.path.join(til_dir, f"{title_to_filename(args[0])}.md")
+    if os.path.isfile(til_path):
+        print(f"ERROR: {til_path} already exists")
+    else:
+        til_file = open(til_path, 'w')
+        til_file.write(frontmatter_str.replace("<<title>>", args[0]))
+        til_file.close()
 
 
 if __name__ == "__main__":
-    if len(sys.argv[1:]):
-        create_file(sys.argv[1])
+    args_provided = sys.argv[1:]
+    if len(args_provided):
+        create_file(args_provided)
     else:
         print("No title provided")
