dfab5c97061c5bc7d21cc0da7bca973a589af62f
colmjude <colmjude@gmail.com>
2022-10-20 22:13:01 +0100

    Add search to main list page

--- src/javascripts/notes-search.js
+++ src/javascripts/notes-search.js
@@ -2,7 +2,7 @@ import debounce from './helpers/debounce';
 
 //const highlight = (s) => htmlEscape(s).replace(
 const highlight = (s) => s.replace(
-  /b4de2a49c8/g, '<b>'
+  /b4de2a49c8/g, '<b class="app-search-highlight">'
 ).replace(
   /8c94a2ed4b/g, '</b>'
 );
@@ -13,7 +13,7 @@ const sql = `select
 from notes
   join notes_fts on notes.rowid = notes_fts.rowid
 where notes_fts match :search || "*"
-  order by rank limit 10;`
+  order by rank limit 5;`
 
 const searchbox = document.getElementById("searchbox");
 
@@ -38,14 +38,14 @@ fetch(url).then(r => r.json()).then(d => {
     // after a faster one.
     return;
   }
-  console.log(d)
+  console.log(d.length)
   let results = d.map(r => `
-    <div class="result">
-      <h3><a href="${r.url}">${r.title}</a></h3>
-      <p><small>${r.author} - ${r.updated_date}</small></p>
-      <p>${highlight(r.snippet)}</p>
+    <div class="app-search-result__item">
+      <h3 class="cj-heading-s app-search-result__item__heading"><a href="${r.url}">${r.title}</a></h3>
+      <p class="app-search-result__item__meta notes-list__item__meta">${r.author} - ${r.updated_date}</p>
+      <p class="app-search-result__item__snippet">${highlight(r.snippet)}</p>
     </div>
   `).join("");
-  document.getElementById("results").innerHTML = results;
+  document.getElementById("app-search-results").innerHTML = results;
 });
 }, 100); // debounce every 100ms
