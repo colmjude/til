d15eeaccf0fee7fba6479c093d7e7d7208caa5ea
colmjude <colmjude@gmail.com>
2022-10-22 20:55:50 +0100

    Display message if no notes found

--- src/javascripts/notes-search.js
+++ src/javascripts/notes-search.js
@@ -7,6 +7,16 @@ const highlight = (s) => s.replace(
   /8c94a2ed4b/g, '</b>'
 );
 
+function createResultItem (result) {
+  return `
+    <div class="app-search-result__item">
+      <h3 class="cj-heading-s app-search-result__item__heading"><a href="${result.url}">${result.title}</a></h3>
+      <p class="app-search-result__item__meta notes-list__item__meta">${result.author} - ${result.updated_date}</p>
+      <p class="app-search-result__item__snippet">${highlight(result.snippet)}</p>
+    </div>
+  `
+}
+
 const sql = `select
   snippet(notes_fts, -1, 'b4de2a49c8', '8c94a2ed4b', '...', 100) as snippet,
   notes_fts.rank, notes.title, notes.url, notes.author, notes.section, notes.updated_date
@@ -19,7 +29,6 @@ const searchbox = document.getElementById("searchbox");
 
 // Used to avoid race-conditions:
 let requestInFlight = null;
-
 
 searchbox.onkeyup = debounce(() => {
 const q = searchbox.value;
@@ -38,14 +47,11 @@ fetch(url).then(r => r.json()).then(d => {
     // after a faster one.
     return;
   }
-  console.log(d.length)
-  let results = d.map(r => `
-    <div class="app-search-result__item">
-      <h3 class="cj-heading-s app-search-result__item__heading"><a href="${r.url}">${r.title}</a></h3>
-      <p class="app-search-result__item__meta notes-list__item__meta">${r.author} - ${r.updated_date}</p>
-      <p class="app-search-result__item__snippet">${highlight(r.snippet)}</p>
-    </div>
-  `).join("");
+
+  let results = `<p class="cj-body">No notes found.</p>`
+  if (d.length) {
+    results = d.map(createResultItem).join("");
+  }
   document.getElementById("app-search-results").innerHTML = results;
 });
 }, 100); // debounce every 100ms
