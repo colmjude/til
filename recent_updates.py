ad73842c0784b1f39d6264e50e0dd1c8fd3e5304
colmjude <colmjude@gmail.com>
2024-04-05 23:16:37 +0300

    add from date option yo recent updates script

--- recent_updates.py
+++ recent_updates.py
@@ -1,14 +1,34 @@
+import argparse
 from datetime import datetime, timedelta
 
 from application.markdown import initiate_markdown
 from note import Notes, note_index
 
 
-def recent_updates():
-    notes = Notes("docs/", initiate_markdown(note_index()))
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, "%Y-%m-%d")
+    except ValueError:
+        raise argparse.ArgumentTypeError("Invalid date format. Please use yyyy-mm-dd.")
 
-    # Calculate the date one week ago
-    one_week_ago = datetime.now() - timedelta(weeks=1)
+
+def get_default_from_date():
+    return (datetime.now() - timedelta(weeks=1)).strftime("%Y-%m-%d")
+
+
+def recent_updates():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--from",
+        dest="from_date",
+        metavar="YYYY-MM-DD",
+        type=parse_date,
+        default=get_default_from_date(),
+        help="Date to go back to (format: yyyy-mm-dd)",
+    )
+    args = parser.parse_args()
+
+    notes = Notes("docs/", initiate_markdown(note_index()))
 
     # Iterate over the notes
     print("")
@@ -22,7 +42,7 @@ def recent_updates():
                 updated_date_str = note.frontmatter["updated"]
                 # Check if the updated_date is within the last week
                 updated_date = datetime.strptime(updated_date_str, "%Y/%m/%d")
-                if updated_date >= one_week_ago:
+                if updated_date >= args.from_date:
                     # Print the note name and URL
                     print(f"[{note.title}](/notes/{note.parent_category}/{note.slug})")
 
